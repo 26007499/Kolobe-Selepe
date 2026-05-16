ac8f00570604ac29440009ecbb8e000e0439bc73
unknown <26007499@mvula.univen.ac.za>
2026-05-16 12:08:19 +0200

    First commit

---
+++ src/main/java/com/mycompany/demo45/Demo45.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo45;
+
+/**
+ *
+ * @author Kolobe
+ */
+public class Demo45 {
+
+    public static void main(String[] args) {
+        System.out.println("Kolobe Selepe");
+        
+    }
+}
